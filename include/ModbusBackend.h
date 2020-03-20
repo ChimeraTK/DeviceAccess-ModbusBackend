4b811650e6a5fb129188deeb3107e396700690af
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2020-03-20 14:41:28 +0100

    Disable merge requests until this is tested.

--- include/ModbusBackend.h
+++ include/ModbusBackend.h
@@ -37,6 +37,8 @@ extern std::mutex modubs_mutex;
 
     virtual void open() override;
     virtual void close() override;
+    //\ToDo: Check if it is possible/works. For now just do not allow merge requests.
+    virtual bool canMergeRequests() const override { return false; }
 
     virtual void read(uint8_t bar, uint32_t address, int32_t* data,  size_t sizeInBytes) override;
     virtual void write(uint8_t bar, uint32_t address, int32_t const* data,  size_t sizeInBytes) override;
